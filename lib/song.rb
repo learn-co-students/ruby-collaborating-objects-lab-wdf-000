d60dab01845958375f364a2e5adc4f19589a7ed0
blessedsoy <unmigong@gmail.com>
2016-06-27 12:19:24 -0400

    Collaborating Objects Lab

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  attr_accessor :artist, :name
+
+  def initialize (name)
+    @name = name
+  end
+
+  def self.new_by_filename(file_name)
+    song = self.new(file_name.split(" - ")[1])
+    song.artist = Artist.find_or_create_by_name(file_name.split(" - ")[0])
+    song
+  end
+
+
+end
