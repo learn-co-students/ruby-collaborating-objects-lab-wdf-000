659615ed6af33742845f0826eaaec20ce47313d4
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-07-21 15:01:31 -0400

    updated objects

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,6 +27,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
+    #all.find{|artist|}
 
     if artist = @@all.detect do |artist_name|
         artist_name.name == name
@@ -47,4 +48,43 @@ class Artist
   end
 
 end
+
+# class Artist
+#   @@all = []
+#   attr_accessor :name, :songs
+
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#   end
+
+#   def self.all
+#     @@all    
+#   end
+
+#   def add_song(song)
+#     @songs << song
+#   end
+
+#   def self.find_or_create_by_name(name)
+#     self.find(name) ? self.find(name) : self.create(name)
+#   end
+
+#   def self.find(name)
+#     self.all.find {|artist| artist.name == name }
+#   end
+
+
+#   def self.create(name)
+#     self.new(name).tap {|artist| artist.save}
+#   end
+
+#   def save
+#     @@all << self
+#   end
+
+#   def print_songs
+#     songs.each {|song| puts song.name}
+#   end
+# end
 
