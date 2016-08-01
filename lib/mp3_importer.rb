016adaca99c7e0fb2d8b8410d6139cce31873e8b
Wendalf <guangw.xu@gmail.com>
2016-08-01 17:46:22 -0400

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,15 +13,11 @@ class MP3Importer
 	end
 
 	def import
-		# self.files.each do |file|
-		# 	artist_name = file.split(" - ")[0]
-		# 	song_name = file.split(" - ")[1]
-		# 	# binding.pry
-		# 	Artist.find_or_create_by_name(artist_name).add_song(song_name)
-		# end
-
-		self.files.each do |filename|
-			Song.new_by_filename(filename)
+		self.files.each do |file|
+			artist_name = file.split(" - ")[0]
+			song_name = file.split(" - ")[1]
+			# binding.pry
+			Artist.find_or_create_by_name(artist_name).add_song(song_name)
 		end
 	end
 
